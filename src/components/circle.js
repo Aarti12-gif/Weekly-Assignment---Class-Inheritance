c1211bc1ed7968691b22db9a21b5e67d6a73b020
Aarti12-gif <81165320+Aarti12-gif@users.noreply.github.com>
2021-05-09 12:02:49 +0530

    Commit

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,10 @@
 
 
 // export class using module.exports
+import React from 'react';
+import ReactDom from 'react-dom';
+class Circle extends Shape{
+constructor(calcArea,drawShape){
+    
+}
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,24 @@
 // declare class
 
 // export class using module.exports
+import React from 'react';
+import ReactDom from 'react-dom';
+class Shape{
+    constructor(height,width){
+        this.height=height;
+        this.width=width;
+        this.color="red";
+    }
+    get area(){
+      return this.calcArea();  
+    }
+    get draw(){
+        return this.drawShape();
+    }
+    calcArea(){
+        return this.height*this.width;
+    }
+    drawShape(){
+        return circle;
+    }
+}
