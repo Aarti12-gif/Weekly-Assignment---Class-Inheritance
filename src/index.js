167803ccc47b7d268b2699d459e7a6864d550f8c
Aarti12-gif <81165320+Aarti12-gif@users.noreply.github.com>
2021-05-11 18:42:38 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-const {Circle} = require('./components/circle');
-let CircleObj=new Circle();
-console.log(CircleObj.calculateArea);
+// const {Circle} = require('./components/circle');
+// let CircleObj=new Circle();
+// console.log(CircleObj.calculateArea);
 ReactDOM.render(<App />, document.getElementById("root"));
